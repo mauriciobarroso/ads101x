1a90881e1be45df070566a5f709d2eb787290005
mauriciobarroso <mauriciobarroso1990@gmail.com>
2023-12-07 19:13:51 -0400

    implemented all functions

--- ads101x.c
+++ ads101x.c
@@ -35,18 +35,548 @@
 /* Includes ------------------------------------------------------------------*/
 #include "ads101x.h"
 
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+
 /* Private macros ------------------------------------------------------------*/
+#define NOP() asm volatile ("nop")
 
 /* External variables --------------------------------------------------------*/
 
 /* Private typedef -----------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
+static const char *TAG = "ads101x";
 
 /* Private function prototypes -----------------------------------------------*/
+/**
+ * @brief Function that implements the default I2C read transaction
+ *
+ * @param reg_addr : Register address to be read
+ * @param reg_data : Pointer to the data to be read from reg_addr
+ * @param data_len : Length of the data transfer
+ * @param intf     : Pointer to the interface descriptor
+ *
+ * @return 0 if successful, non-zero otherwise
+ */
+static int8_t i2c_read(uint8_t reg_addr, uint16_t *reg_data, void *intf);
+
+/**
+ * @brief Function that implements the default I2C write transaction
+ *
+ * @param reg_addr : Register address to be written
+ * @param reg_data : Data to be written to reg_addr
+ * @param data_len : Length of the data transfer
+ * @param intf     : Pointer to the interface descriptor
+ *
+ * @return 0 if successful, non-zero otherwise
+ */
+static int8_t i2c_write(uint8_t reg_addr, const uint16_t reg_data,
+		                    void *intf);
+/**
+ * @brief Function that implements a micro seconds delay
+ *
+ * @param period_us: Time in us to delay
+ */
+static void delay_us(uint32_t period_us);
 
 /* Exported functions definitions --------------------------------------------*/
+/**
+ * @brief Function to initialize a ADS101x instance
+ */
+esp_err_t ads101x_init(ads101x_t *const me, i2c_bus_t *i2c_bus, uint8_t dev_addr,
+		                 i2c_bus_read_t read, i2c_bus_write_t write) {
+	/* Print initializing message */
+	ESP_LOGI(TAG, "Initializing instance...");
+
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Add device to bus */
+	ret = i2c_bus_add_dev(i2c_bus, dev_addr, "ads101x", NULL, NULL);
+
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to add device");
+		return ret;
+	}
+
+	/**/
+	me->i2c_dev = &i2c_bus->devs.dev[i2c_bus->devs.num - 1]; /* todo: write function to get the dev from name */
+
+	/* Print successful initialization message */
+	ESP_LOGI(TAG, "Instance initialized successfully");
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads a specific single-ended ADC channel.
+ */
+esp_err_t ads101x_read_single_ended(ads101x_t *const me,
+		                                ads101x_channel_t channel,
+																		int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Check channel argument */
+	if (me->model == ADS101X_MODEL_5) { /* ADS1015 */
+		if (channel > 3) {
+			ESP_LOGE(TAG, "Failed to select channel, must be less than 3");
+			return ESP_FAIL;
+		}
+	}
+	else { /* ADS1013 and ADS1014 */
+		if (channel > 1) {
+			ESP_LOGE(TAG, "Failed to select channel, must be less than 1");
+			return ESP_FAIL;
+		}
+	}
+
+	/* Perform a oneshot ADC reading */
+	ret = ads101x_start_reading(me, channel, ADS101X_MODE_ONESHOT);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Check if the conversion is complete */
+	bool conversion_is_complete = false;
+
+	do {
+		delay_us(5 * 1000); /* Wait for 5 ms */
+		ads101x_conversion_complete(me, &conversion_is_complete);
+	} while (!conversion_is_complete);
+
+	/* Get the las ADC conversion result */
+	ret = ads101x_get_last_conversion_results(me, adc_result);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN0) and N
+ *				(AIN1) input.
+ */
+esp_err_t ads101x_read_differential_0_1(ads101x_t *const me,
+		                                    int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Perform a oneshot ADC reading */
+	ret = ads101x_start_reading(me, ADS101X_REG_CONFIG_MUX_DIFF_0_1, ADS101X_MODE_ONESHOT);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Check if the conversion is complete */
+	bool conversion_is_complete = false;
+
+	do {
+		delay_us(5 * 1000); /* Wait for 5 ms */
+		ads101x_conversion_complete(me, &conversion_is_complete);
+	} while (!conversion_is_complete);
+
+	/* Get the las ADC conversion result */
+	ret = ads101x_get_last_conversion_results(me, adc_result);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN0) and N
+ *				(AIN3) input.
+ */
+esp_err_t ads101x_read_differential_0_3(ads101x_t *const me,
+		                                    int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Perform a oneshot ADC reading */
+	ret = ads101x_start_reading(me, ADS101X_REG_CONFIG_MUX_DIFF_0_3, ADS101X_MODE_ONESHOT);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Check if the conversion is complete */
+	bool conversion_is_complete = false;
+
+	do {
+		delay_us(5 * 1000); /* Wait for 5 ms */
+		ads101x_conversion_complete(me, &conversion_is_complete);
+	} while (!conversion_is_complete);
+
+	/* Get the las ADC conversion result */
+	ret = ads101x_get_last_conversion_results(me, adc_result);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN1) and N
+ *				(AIN3) input.
+ */
+esp_err_t ads101x_read_differential_1_3(ads101x_t *const me,
+		                                    int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Perform a oneshot ADC reading */
+	ret = ads101x_start_reading(me, ADS101X_REG_CONFIG_MUX_DIFF_1_3, ADS101X_MODE_ONESHOT);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Check if the conversion is complete */
+	bool conversion_is_complete = false;
+
+	do {
+		delay_us(5 * 1000); /* Wait for 5 ms */
+		ads101x_conversion_complete(me, &conversion_is_complete);
+	} while (!conversion_is_complete);
+
+	/* Get the las ADC conversion result */
+	ret = ads101x_get_last_conversion_results(me, adc_result);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN2) and N
+ *				(AIN3) input.
+ */
+esp_err_t ads101x_read_differential_2_3(ads101x_t *const me,
+		                                    int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Perform a oneshot ADC reading */
+	ret = ads101x_start_reading(me, ADS101X_REG_CONFIG_MUX_DIFF_2_3, ADS101X_MODE_ONESHOT);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Check if the conversion is complete */
+	bool conversion_is_complete = false;
+
+	do {
+		delay_us(5 * 1000); /* Wait for 5 ms */
+		ads101x_conversion_complete(me, &conversion_is_complete);
+	} while (!conversion_is_complete);
+
+	/* Get the las ADC conversion result */
+	ret = ads101x_get_last_conversion_results(me, adc_result);
+
+	if (ret != ESP_OK) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that sets up the comparator to operator in basic mode,
+ *        causing the ALRT/RDY pin to assert when the ADC value exceeeds the
+ *        specified value.
+ */
+esp_err_t ads101x_start_comparator_single_ended(ads101x_t *const me,
+		                                            ads101x_channel_t channel,
+																								int16_t threshold) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Fill the ADS101x configuration value */
+	uint16_t config = ADS101X_REG_CONFIG_CQUE_1CONV |   /* Comparator enabled and
+	                                                       asserts on 1 match */
+			              ADS101X_REG_CONFIG_CLAT_LATCH |   /* Latching mode */
+										ADS101X_REG_CONFIG_CPOL_ACTVLOW | /* Alert/ready active low */
+										ADS101X_REG_CONFIG_CMODE_TRAD |   /* Traditional comparator */
+										ADS101X_REG_CONFIG_MODE_CONTIN;   /* Continuos conversion
+										                                     mode */
+
+	/* Set PGA/voltage range */
+	config |= me->gain;
+
+	/* Set data rate */
+	config |= me->data_rate;
+
+	/* Set channel */
+	config |= channel;
+
+	/* Set the high threshold register */
+	if (i2c_write(ADS101X_REG_POINTER_HITHRESH, threshold << me->bit_shift, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	/* Set the new ADC configuration */
+	if (i2c_write(ADS101X_REG_POINTER_CONFIG, config, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that reads the last conversion results without changin the
+ *        configuration value.
+ */
+esp_err_t ads101x_get_last_conversion_results(ads101x_t *const me,
+		                                          int16_t *adc_result) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Read the conversion result */
+	uint16_t result;
+
+	if (i2c_read(ADS101X_REG_POINTER_CONVERT, &result, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	result >>= me->bit_shift;
+
+	if (me->bit_shift == 0) {
+		*adc_result = (int16_t)result;
+	}
+	else {
+		/* Shift 12-bit results right 4 bits for the ADS101x,
+		 * making sure we keep the sign bit intact */
+		if (result > 0x07FF) {
+			/* Negative number, extend the sign to 16th bit */
+			result |= 0xF000;
+		}
+
+		*adc_result = (int16_t)result;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that computes the voltage value from ADC raw value
+ */
+float ads101x_compute_volts(ads101x_t *const me, int16_t counts) {
+	float fs_range;
+
+	switch (me->gain) {
+		case ADS101X_GAIN_TWOTHIRDS:
+			fs_range = 6.144f;
+			break;
+		case ADS101X_GAIN_ONE:
+			fs_range = 4.096f;
+			break;
+		case ADS101X_GAIN_TWO:
+			fs_range = 2.048f;
+			break;
+		case ADS101X_GAIN_FOUR:
+			fs_range = 1.024f;
+			break;
+		case ADS101X_GAIN_EIGHT:
+			fs_range = 0.512f;
+			break;
+		case ADS101X_GAIN_SIXTEEN:
+			fs_range = 0.256f;
+			break;
+		default:
+			fs_range = 0.0f;
+	}
+
+	return (counts * (fs_range / (32768 >> me->bit_shift)));
+}
+
+
+/**
+ * @brief Function that sets the ADS101x gain
+ */
+void ads101x_set_gain(ads101x_t *const me, ads101x_gain_t gain) {
+	me->gain = gain;
+}
+
+/**
+ * @brief Function that get the ADS101x gain
+ */
+ads101x_gain_t ads101x_get_gain(ads101x_t *const me) {
+	return me->gain;
+}
+
+/**
+ * @brief Function that sets the ADS101x data rate
+ */
+void ads101x_set_data_rate(ads101x_t *const me, ads101x_data_rate_t data_rate) {
+	me->data_rate = data_rate;
+}
+
+/**
+ * @brief Function that gets ADS101x data rate
+ */
+ads101x_data_rate_t ads101x_get_data_rate(ads101x_t *const me) {
+	return me->data_rate;
+}
+
+/**
+ * @brief Function that stars the conversion function
+ */
+esp_err_t ads101x_start_reading(ads101x_t *const me, uint16_t mux,
+		                            ads101x_mode_t mode) {
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Fill the ADS101x configuration value */
+	uint16_t config = ADS101X_REG_CONFIG_CQUE_1CONV |   /* Set CQUE to any value
+	                                                       other than None so we
+	                                                       can use it in RDY mode
+	                                                       */
+			              ADS101X_REG_CONFIG_CLAT_NONLAT |  /* Non-latching */
+										ADS101X_REG_CONFIG_CPOL_ACTVLOW | /* ALERT/RDY active low */
+										ADS101X_REG_CONFIG_CMODE_TRAD;    /* Traditional comparator */
+
+	/* Configure the reading mode */
+	if (mode == ADS101X_MODE_CONTINUOS) {
+		config |= ADS101X_REG_CONFIG_MODE_CONTIN;
+	}
+	else {
+		config |= ADS101X_REG_CONFIG_MODE_CONTIN;
+	}
+
+	/* Set PGA/voltage range */
+	config |= me->gain;
+
+	/* Set data rate */
+	config |= me->data_rate;
+
+	/* Set channel */
+	config |= mux;
+
+	/* Set to start a single-conversion */
+	config |= ADS101X_REG_CONFIG_OS_SINGLE;
+
+	/* Set the new ADC configuration */
+	if (i2c_write(ADS101X_REG_POINTER_CONFIG, config, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	/* Set ALERT/RDY to RDY mode */
+	if (i2c_write(ADS101X_REG_POINTER_HITHRESH, 0x8000, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	if (i2c_write(ADS101X_REG_POINTER_HITHRESH, 0x0000, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function that check if the ADC reading is complete
+ */
+esp_err_t ads101x_conversion_complete(ads101x_t *const me, bool *is_complete) {
+
+	/* Variable to return error code */
+	esp_err_t ret = ESP_OK;
+
+	/* Check if the device is performing a conversion */
+	uint16_t rx_data = 0;
+
+	if (i2c_read(ADS101X_REG_POINTER_CONFIG & 0x8000, &rx_data, me->i2c_dev) < 0) {
+		return ESP_FAIL;
+	}
+
+	*is_complete = (bool)rx_data;
+
+	/* Return ESP_OK */
+	return ret;
+}
 
 /* Private function definitions ----------------------------------------------*/
+
+/**
+ * @brief Function that implements the default I2C read transaction
+ */
+static int8_t i2c_read(uint8_t reg_addr, uint16_t *reg_data, void *intf) {
+	int8_t ret = 0;
+	i2c_bus_dev_t *dev = (i2c_bus_dev_t *)intf;
+
+	uint8_t buf[2] = {0};
+
+	ret =  dev->read(reg_addr ? &reg_addr : NULL, reg_addr ? 1 : 0, buf, 2, dev);
+
+	if (ret < 0) {
+		return ret;
+	}
+
+	*reg_data = (uint16_t)((buf[0] << 8) | buf[1]);
+
+	/* Return for success */
+	return ret;
+}
+/**
+ * @brief Function that implements the default I2C write transaction
+ */
+static int8_t i2c_write(uint8_t reg_addr, const uint16_t reg_data,
+		                    void *intf) {
+	int8_t ret = 0;
+	i2c_bus_dev_t *dev = (i2c_bus_dev_t *)intf;
+
+	uint8_t buf[2] = {(uint8_t)((reg_data & 0xFF00) >> 8),
+			(uint8_t)(reg_data & 0x00FF)};
+
+	ret = dev->write(&reg_addr, 1, buf, 2, dev);
+
+	if (ret < 0) {
+		return ret;
+	}
+
+	/* Return for success */
+	return ret;
+}
+/**
+ * @brief Function that implements a micro seconds delay
+ */
+static void delay_us(uint32_t period_us) {
+	uint64_t m = (uint64_t)esp_timer_get_time();
+
+  if (period_us) {
+  	uint64_t e = (m + period_us);
+
+  	if (m > e) { /* overflow */
+  		while ((uint64_t)esp_timer_get_time() > e) {
+  			NOP();
+  		}
+  	}
+
+  	while ((uint64_t)esp_timer_get_time() < e) {
+  		NOP();
+  	}
+  }
+}
 
 /***************************** END OF FILE ************************************/
--- include/ads101x.h
+++ include/ads101x.h
@@ -43,14 +43,300 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "i2c_bus.h"
 
 /* Exported Macros -----------------------------------------------------------*/
+/* ADS101x I2C device address */
+#define ADS101X_ADDRESS	0x48 /* ADDR pin connected to GND */
+
+/* Pointer registers */
+#define ADS101X_REG_POINTER_MASK			0x03 /* Point mask */
+#define ADS101X_REG_POINTER_CONVERT		0x00 /* Conversion */
+#define ADS101X_REG_POINTER_CONFIG		0x01 /* Configuration */
+#define ADS101X_REG_POINTER_LOWTHRESH	0x02 /* Low threshold */
+#define ADS101X_REG_POINTER_HITHRESH	0x03 /* High threshold */
+
+/* Configuration registers */
+#define ADS101X_REG_CONFIG_OS_MASK			0x8000 /* OS Mask */
+#define ADS101X_REG_CONFIG_OS_SINGLE		0x8000 /* Write: Set to start a
+																									single-conversion */
+#define ADS101X_REG_CONFIG_OS_BUSY			0x0000 /* Read: Bit = 0 when conversion
+																									is in progress */
+#define ADS101X_REG_CONFIG_OS_NOTBUSY		0x8000 /* Read: Bit = 1 when device is
+																									not performing a conversion */
+#define ADS101X_REG_CONFIG_MUX_MASK			0x7000 /* Mux Mask */
+#define ADS101X_REG_CONFIG_MUX_DIFF_0_1	0x0000 /* Differential P = AIN0,
+																									N = AIN1 (default) */
+#define ADS101X_REG_CONFIG_MUX_DIFF_0_3	0x1000 /* Differential P = AIN0,
+																									N = AIN3 */
+#define ADS101X_REG_CONFIG_MUX_DIFF_1_3	0x2000 /* Differential P = AIN1,
+																									N = AIN3 */
+#define ADS101X_REG_CONFIG_MUX_DIFF_2_3	0x3000 /* Differential P = AIN2,
+																									N = AIN3 */
+#define ADS101X_REG_CONFIG_MUX_SINGLE_0	0x4000 /* Single-ended AIN0 */
+#define ADS101X_REG_CONFIG_MUX_SINGLE_1	0x5000 /* Single-ended AIN1 */
+#define ADS101X_REG_CONFIG_MUX_SINGLE_2	0x6000 /* Single-ended AIN2 */
+#define ADS101X_REG_CONFIG_MUX_SINGLE_3	0x7000 /* Single-ended AIN3 */
+#define ADS101X_REG_CONFIG_PGA_MASK			0x0E00 /* PGA Mask */
+#define ADS101X_REG_CONFIG_PGA_6_144V		0x0000 /* +/-6.144V range = Gain 2/3 */
+#define ADS101X_REG_CONFIG_PGA_4_096V		0x0200 /* +/-4.096V range = Gain 1 */
+#define ADS101X_REG_CONFIG_PGA_2_048V		0x0400 /* +/-2.048V range = Gain 2 */
+#define ADS101X_REG_CONFIG_PGA_1_024V		0x0600 /* +/-1.024V range = Gain 4 */
+#define ADS101X_REG_CONFIG_PGA_0_512V		0x0800 /* +/-0.512V range = Gain 8 */
+#define ADS101X_REG_CONFIG_PGA_0_256V		0x0A00 /* +/-0.256V range = Gain 16 */
+#define ADS101X_REG_CONFIG_MODE_MASK		0x0100   /* Mode Mask */
+#define ADS101X_REG_CONFIG_MODE_CONTIN	0x0000 /* Continuous conversion mode */
+#define ADS101X_REG_CONFIG_MODE_SINGLE	0x0100 /* Power-down single-shot mode */
+#define ADS101X_REG_CONFIG_RATE_MASK		0x00E0 /* Data Rate Mask */
+#define ADS101X_REG_CONFIG_CMODE_MASK		0x0010 /* CMode Mask */
+#define ADS101X_REG_CONFIG_CMODE_TRAD		0x0000 /* Traditional comparator with
+                                                  hysteresis (default) */
+#define ADS101X_REG_CONFIG_CMODE_WINDOW	0x0010 /* Window comparator */
+#define ADS101X_REG_CONFIG_CPOL_MASK		0x0008 /* CPol Mask */
+#define ADS101X_REG_CONFIG_CPOL_ACTVLOW	0x0000 /* ALERT/RDY pin is low when
+																									active */
+#define ADS101X_REG_CONFIG_CPOL_ACTVHI	0x0008 /* ALERT/RDY pin is high when
+																									active */
+#define ADS101X_REG_CONFIG_CLAT_MASK		0x0004 /* Determines if ALERT/RDY pin
+																									latches once asserted */
+#define ADS101X_REG_CONFIG_CLAT_NONLAT	0x0000 /* Non-latching comparator */
+#define ADS101X_REG_CONFIG_CLAT_LATCH		0x0004 /* Latching comparator */
+#define ADS101X_REG_CONFIG_CQUE_MASK		0x0003 /* CQue Mask */
+#define ADS101X_REG_CONFIG_CQUE_1CONV		0x0000 /* Assert ALERT/RDY after one
+																									conversions */
+#define ADS101X_REG_CONFIG_CQUE_2CONV		0x0001 /* Assert ALERT/RDY after two
+																									conversions */
+#define ADS101X_REG_CONFIG_CQUE_4CONV		0x0002 /* Assert ALERT/RDY after four
+																									conversions */
+#define ADS101X_REG_CONFIG_CQUE_NONE		0x0003 /* Disable the comparator and put
+																									ALERT/RDY in high state */
 
 /* Exported typedef ----------------------------------------------------------*/
+typedef enum {
+	ADS101X_MODEL_3, /* ADS1013 model */
+	ADS101X_MODEL_4, /* ADS1014 model */
+	ADS101X_MODEL_5, /* ADS1015 model */
+} ads101x_model_t;
+
+/* Gain settings */
+typedef enum {
+	ADS101X_GAIN_TWOTHIRDS = ADS101X_REG_CONFIG_PGA_6_144V,
+	ADS101X_GAIN_ONE = ADS101X_REG_CONFIG_PGA_4_096V,
+	ADS101X_GAIN_TWO = ADS101X_REG_CONFIG_PGA_2_048V,
+	ADS101X_GAIN_FOUR = ADS101X_REG_CONFIG_PGA_1_024V,
+	ADS101X_GAIN_EIGHT = ADS101X_REG_CONFIG_PGA_0_512V,
+	ADS101X_GAIN_SIXTEEN = ADS101X_REG_CONFIG_PGA_0_256V
+} ads101x_gain_t;
+
+/* Data rates */
+typedef enum {
+	ADS101X_DATA_RATE_128SPS =	0x0000,
+	ADS101X_DATA_RATE_250SPS =	0x0020,
+	ADS101X_DATA_RATE_490SPS =	0x0040,
+	ADS101X_DATA_RATE_920SPS =	0x0060,
+	ADS101X_DATA_RATE_1600SPS =	0x0080,
+	ADS101X_DATA_RATE_2400SPS =	0x00A0,
+	ADS101X_DATA_RATE_3300SPS =	0x00C0
+} ads101x_data_rate_t;
+
+/* Mode setting */
+typedef enum {
+	ADS101X_MODE_ONESHOT,
+	ADS101X_MODE_CONTINUOS,
+} ads101x_mode_t;
+
+/* Channels */
+typedef enum {
+	ADS101X_CHANNEL_0 = ADS101X_REG_CONFIG_MUX_SINGLE_0,
+	ADS101X_CHANNEL_1 = ADS101X_REG_CONFIG_MUX_SINGLE_1,
+	ADS101X_CHANNEL_2 = ADS101X_REG_CONFIG_MUX_SINGLE_2,
+	ADS101X_CHANNEL_3 = ADS101X_REG_CONFIG_MUX_SINGLE_3
+} ads101x_channel_t;
+
+typedef struct {
+	i2c_bus_dev_t *i2c_dev;
+	ads101x_model_t model;
+	ads101x_gain_t gain;
+	ads101x_data_rate_t data_rate;
+	uint8_t bit_shift;
+} ads101x_t;
 
 /* Exported variables --------------------------------------------------------*/
 
 /* Exported functions prototypes ---------------------------------------------*/
+/**
+ * @brief Function that initializes a ADS101x instance
+ *
+ * @param me       : Pointer to a ads101x_t instance
+ * @param i2c_bus  : Pointer to a structure with the data to initialize the
+ * 								   I2C device
+ * @param dev_addr : I2C device address
+ * @param read     : Pointer to I2C read function
+ * @param write    : Pointer to I2C write function
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_init(ads101x_t *const me, i2c_bus_t *i2c_bus, uint8_t dev_addr,
+		                 i2c_bus_read_t read, i2c_bus_write_t write);
+
+/**
+ * @brief Function that reads a specific single-ended ADC channel.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param channel    : ADC channel to be read
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_read_single_ended(ads101x_t *const me,
+		                                ads101x_channel_t channel,
+																		int16_t *adc_result);
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN0) and N
+ *				(AIN1) input.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_read_differential_0_1(ads101x_t *const me,
+		                                    int16_t *adc_result);
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN0) and N
+ *				(AIN3) input.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_read_differential_0_3(ads101x_t *const me,
+		                                    int16_t *adc_result);
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN1) and N
+ *				(AIN3) input.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_read_differential_1_3(ads101x_t *const me,
+		                                    int16_t *adc_result);
+
+/**
+ * @brief Function that reads the voltage difference between the P (AIN2) and N
+ *				(AIN3) input.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_read_differential_2_3(ads101x_t *const me,
+		                                    int16_t *adc_result);
+
+/**
+ * @brief Function that sets up the comparator to operator in basic mode,
+ *        causing the ALRT/RDY pin to assert when the ADC value exceeeds the
+ *        specified value.
+ *
+ * @param me        : Pointer to a ads101x_t instance
+ * @param channel   : ADC channel to be read
+ * @param threshold : Comparator threshold value
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_start_comparator_single_ended(ads101x_t *const me,
+		                                            ads101x_channel_t channel,
+																								int16_t threshold);
+
+/**
+ * @brief Function that reads the last conversion results without changin the
+ *        configuration value.
+ *
+ * @param me         : Pointer to a ads101x_t instance
+ * @param adc_result : ADC conversion result
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_get_last_conversion_results(ads101x_t *const me,
+		                                          int16_t *adc_result);
+
+/**
+ * @brief Function that computes the voltage value from ADC raw value
+ *
+ * @param me     : Pointer to a ads101x_t instance
+ * @param counts : ADC reading in raw counts
+ *
+ * @return Voltage value
+ */
+float ads101x_compute_volts(ads101x_t *const me, int16_t counts);
+
+/**
+ * @brief Function that sets the ADS101x gain
+ *
+ * @param me   : Pointer to a ads101x_t instance
+ * @param gain : ADS101x gain: 2/3, 1, 2, 4, 8 and 16
+ */
+void ads101x_set_gain(ads101x_t *const me, ads101x_gain_t gain);
+
+/**
+ * @brief Function that gets the ADS101x gain
+ *
+ * @param me : Pointer to a ads101x_t instance
+ *
+ * @return ADS101x gain
+ */
+ads101x_gain_t ads101x_get_gain(ads101x_t *const me);
+
+/**
+ * @brief Function that sets the ADS101x data rate
+ *
+ * @param me        : Pointer to a ads101x_t instance
+ * @param data_rate : ADS101x data rate: 128, 250, 490, 920, 1600, 2400, 3300 SPS
+ */
+void ads101x_set_data_rate(ads101x_t *const me, ads101x_data_rate_t data_rate);
+
+/**
+ * @brief Function that gets ADS101x data rate
+ *
+ * @param me : Pointer to a ads101x_t instance
+ *
+ * @return ADS101x data rate
+ */
+ads101x_data_rate_t ads101x_get_data_rate(ads101x_t *const me);
+
+/**
+ * @brief Function that stars the conversion function
+ *
+ * @param me   : Pointer to a ads101x_t instance
+ * @param mux  : MUX field value
+ * @param mode : ADS101x reading mode
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_start_reading(ads101x_t *const me, uint16_t mux,
+		                            ads101x_mode_t mode);
+
+/**
+ * @brief Function that check if the ADC reading is complete
+ *
+ * @param me          : Pointer to a ads101x_t instance
+ * @param is_complete : Pointer to value to indicate if a ADC conversion is
+ *                      complete
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t ads101x_conversion_complete(ads101x_t *const me, bool *is_complete);
+
 
 #ifdef __cplusplus
 }
